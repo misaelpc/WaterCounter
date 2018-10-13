91e262891ca84b1e17ed5a57bdcd5139b81a69ff
Misael Pérez Chamorro <misaelperezchamorro@Misaels-MacBook-Pro.local>
2018-10-13 13:56:05 -0500

    first commit

---
+++ WaterCounter/ActionButton.swift
@@ -0,0 +1,21 @@
+//
+//  ActionButton.swift
+//  WaterCounter
+//
+//  Created by Misael Pérez Chamorro on 10/13/18.
+//  Copyright © 2018 Misael Pérez Chamorro. All rights reserved.
+//
+
+import UIKit
+
+class ActionButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ WaterCounter/CounterView.swift
@@ -0,0 +1,21 @@
+//
+//  CounterView.swift
+//  WaterCounter
+//
+//  Created by Misael Pérez Chamorro on 10/13/18.
+//  Copyright © 2018 Misael Pérez Chamorro. All rights reserved.
+//
+
+import UIKit
+
+class CounterView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
